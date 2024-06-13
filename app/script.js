9e593419e42e4cabb59a30dd9781dc202a84925b
spl3ndid <spl3ndid@pm.me>
2024-06-13 18:40:46 +0400

    first commit

---
+++ app/script.js
@@ -0,0 +1,119 @@
+const calculator = {
+    displayValue: '0',
+    firstOperand: null,
+    waitingForSecondOperand: false,
+    operator: null,
+};
+
+function updateDisplay() {
+    const display = document.querySelector('.calculator-screen');
+    display.value = calculator.displayValue;
+}
+
+function handleNumber(number) {
+    const { displayValue, waitingForSecondOperand } = calculator;
+
+    if (waitingForSecondOperand === true) {
+        calculator.displayValue = number;
+        calculator.waitingForSecondOperand = false;
+    } else {
+        calculator.displayValue = displayValue === '0' ? number : displayValue + number;
+    }
+}
+
+function handleOperator(nextOperator) {
+    const { firstOperand, displayValue, operator } = calculator;
+    const inputValue = parseFloat(displayValue);
+
+    if (operator && calculator.waitingForSecondOperand) {
+        calculator.operator = nextOperator;
+        return;
+    }
+
+    if (firstOperand == null && !isNaN(inputValue)) {
+        calculator.firstOperand = inputValue;
+    } else if (operator) {
+        const result = performCalculation[operator](firstOperand, inputValue);
+
+        calculator.displayValue = String(result);
+        calculator.firstOperand = result;
+    }
+
+    calculator.waitingForSecondOperand = true;
+    calculator.operator = nextOperator;
+}
+
+const performCalculation = {
+    '/': (firstOperand, secondOperand) => firstOperand / secondOperand,
+    '*': (firstOperand, secondOperand) => firstOperand * secondOperand,
+    '+': (firstOperand, secondOperand) => firstOperand + secondOperand,
+    '-': (firstOperand, secondOperand) => firstOperand - secondOperand,
+    '=': (firstOperand, secondOperand) => secondOperand,
+};
+
+function resetCalculator() {
+    calculator.displayValue = '0';
+    calculator.firstOperand = null;
+    calculator.waitingForSecondOperand = false;
+    calculator.operator = null;
+}
+
+function backspace() {
+    calculator.displayValue = calculator.displayValue.slice(0, -1) || '0';
+}
+
+updateDisplay();
+
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', event => {
+    const { target } = event;
+    const { value } = target;
+    if (!target.matches('button')) {
+        return;
+    }
+
+    switch (value) {
+        case '+':
+        case '-':
+        case '*':
+        case '/':
+        case '=':
+            handleOperator(value);
+            break;
+        case '.':
+            if (!calculator.displayValue.includes(value)) {
+                calculator.displayValue += value;
+            }
+            break;
+        case 'all-clear':
+            resetCalculator();
+            break;
+        default:
+            if (Number.isInteger(parseFloat(value))) {
+                handleNumber(value);
+            }
+    }
+
+    updateDisplay();
+});
+
+// Add keyboard support
+document.addEventListener('keydown', event => {
+    const key = event.key;
+    if (key >= 0 && key <= 9) {
+        handleNumber(key);
+    } else if (key === '.') {
+        if (!calculator.displayValue.includes(key)) {
+            calculator.displayValue += key;
+        }
+    } else if (key === '=' || key === 'Enter') {
+        handleOperator('=');
+    } else if (key === '+' || key === '-' || key === '*' || key === '/') {
+        handleOperator(key);
+    } else if (key === 'Escape') {
+        resetCalculator();
+    } else if (key === 'Backspace') {
+        backspace();
+    }
+    updateDisplay();
+});
